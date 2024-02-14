77a7cf27546d2e16b4e9dccefaf569ebbdefb88e
JhonDiazArango <jhon.diaz512@pascualbravo.edu.co>
2024-02-14 02:26:32 +0000

    Updated repo operations

--- Sem_01/repo_private.py
+++ Sem_01/repo_private.py
@@ -2,11 +2,11 @@
 
 import os
 print(os.path.dirname(os.path.realpath(__file__)))
-this_dir = os.path.dirname(os.path.realpath(__file__))
+this_dir = os.path.dirname(os.path.realpath("/content/drive/MyDrive/"))
 
 GIT_KEY=""
 
-with open(this_dir + "/repo_keys.txt") as f:
+with open(this_dir + "/MyDrive/repo_keys.txt") as f:
 	for line in f:
 		tups=line.strip().split("=")
 		if tups[0] == "GIT_KEY":
